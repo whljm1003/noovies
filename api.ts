5e65eba194559ce4608ffb0cedd95e43ee3a2717
Jungmin Lee <whljm1003@gmail.com>
2022-01-25 15:45:24 +0900

    Detail Screen part Four

--- api.ts
+++ api.ts
@@ -70,6 +70,12 @@ export const moviesApi: Fetchers<MovieResponse> = {
       `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=1&query=${query}`
     ).then((res) => res.json());
   },
+  detail: ({ queryKey }) => {
+    const [_, id] = queryKey;
+    return fetch(
+      `${BASE_URL}/movie/${id}?api_key=${API_KEY}&append_to_response=videos,images`
+    ).then((res) => res.json());
+  },
 };
 
 export const tvApi: Fetchers<TVResponse> = {
@@ -89,6 +95,12 @@ export const tvApi: Fetchers<TVResponse> = {
     const [_, query] = queryKey;
     return fetch(
       `${BASE_URL}/search/tv?api_key=${API_KEY}&language=en-US&page=1&query=${query}`
+    ).then((res) => res.json());
+  },
+  detail: ({ queryKey }) => {
+    const [_, id] = queryKey;
+    return fetch(
+      `${BASE_URL}/tv/${id}?api_key=${API_KEY}&append_to_response=videos,images`
     ).then((res) => res.json());
   },
 };
